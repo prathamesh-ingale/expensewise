a8ae4131ea40aa895f51a8897d8ecd9e87eac329
Prathamesh Ingale <PRATHAMESH.INGALE024@nmims.in>
2026-08-01 17:11:59 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='expensewise-v1';const FILES=['./','./index.html','./styles.css','./app.js'];self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES))));self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(saved=>saved||fetch(event.request))));
